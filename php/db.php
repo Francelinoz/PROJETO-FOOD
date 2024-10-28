78304714a736aea9f4cbf92b0cb7bba35b2b865b
Francelinoz <miguelsf806@gmail.com>
2024-10-28 19:17:41 -0300

    att-1.1

---
+++ php/db.php
@@ -0,0 +1,14 @@
+<?php
+$host = 'localhost';
+$dbname = 'bugerbliss';
+$user = 'root';
+$pass = '';
+
+try {
+    $pdo = new PDO(dsn: "mysql:host=$host;dbname=$dbname;charset=utf8", username: $user, password: $pass);
+    $pdo->setAttribute(attribute: PDO::ATTR_ERRMODE, value: PDO::ERRMODE_EXCEPTION);
+    //echo "Conexão com o banco de dados estabelecida com sucesso.";
+} catch (PDOException $e) {
+    echo "Falha na conexão com o banco de dados: " . $e->getMessage();
+}
+?>
