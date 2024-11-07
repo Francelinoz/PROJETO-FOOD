21ae59a2a1f2f6dcdb82af5c594102b522f019ad
Eduardo-dp <edomenicalipeixoto@gmail.com>
2024-11-07 20:06:51 -0300

    atualização

--- admin/models/UsuarioModel.php
+++ admin/models/UsuarioModel.php
@@ -6,14 +6,9 @@ class UsuarioModel {
     public function getUsuarios($pdo) {
         try {
             $sql = "SELECT id, email, senha FROM usuarios";
-<<<<<<< Updated upstream
             $result = $pdo->query($sql);   
             return $result->fetchAll(PDO::FETCH_ASSOC);
-=======
-            $result = $pdo->query($sql);
-            return $result->fetchAll(\PDO::FETCH_ASSOC);
->>>>>>> Stashed changes
-            
+
         } catch (PDOException $e) {
 
             echo "Erro ao buscar usuários: " . $e->getMessage();
