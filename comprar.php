47a734fac024a369dfc1e01659f2db9f9bd6035c
Eduardo-dp <edomenicalipeixoto@gmail.com>
2024-12-05 19:37:24 -0300

    att

--- comprar.php
+++ comprar.php
@@ -1,32 +1,36 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pedido</title>
 </head>
 <style>
-    body{
+    body {
         background-image: linear-gradient(#E68023 40%, white);
         background-repeat: no-repeat;
         margin: 0;
         font-family: sans-serif;
 
     }
-    .menu{
+
+    .menu {
         background-color: black;
         padding: 30px;
         text-align: center;
-        
+
     }
-    .ancora{
+
+    .ancora {
         color: aliceblue;
         text-decoration: none;
         font-weight: 200;
         margin: 20px;
         font-size: 18px;
     }
-    .box{
+
+    .box {
         background-color: rgba(0, 0, 0, 0.400);
         color: aliceblue;
         margin: 20px;
@@ -34,35 +38,41 @@
         border-radius: 16px;
         transition: .4s;
     }
-    .box:hover{
+
+    .box:hover {
         background-color: rgba(0, 0, 0, 0.600);
-        
+
     }
-    .box2{
+
+    .box2 {
         background-color: white;
         color: aliceblue;
         margin: 40px;
         width: 400px;
-        
+
         padding: 40px;
         border-radius: 16px;
         transition: .4s;
         box-shadow: 0px 0px 16px #E68023;
     }
-    .label{
+
+    .label {
         font-size: 26px;
     }
-    .centralizado{
+
+    .centralizado {
         background: rgba(0, 0, 0, 0.600);
         color: white;
         padding: 10px;
         border-radius: 6px;
         border: 1px solid rgba(255, 255, 255, 0.400);
     }
-    .centralizado:hover{
+
+    .centralizado:hover {
         cursor: pointer;
     }
-    .centralizado2{
+
+    .centralizado2 {
         background: #E68023;
         color: white;
         width: 100%;
@@ -70,14 +80,16 @@
         border-radius: 16px;
         border: 1px solid rgba(255, 255, 255, 0.400)
     }
-    .centralizado2:hover{
+
+    .centralizado2:hover {
         background: #A84000;
         cursor: pointer;
         box-shadow: 0px 0px 16px #E68023;
     }
 </style>
+
 <body>
-<header>
+    <header>
         <nav>
             <div class="nav-bar">
                 <div class="menu">
@@ -104,10 +116,10 @@
 
                     <input type="submit" class="centralizado" name="adicionar" value="Adicionar"><br><br>
                 </div>
-            
+
                 <div class="box">
 
-              
+
                     <label for="Lanche">Lanche:</label>
                     <label for="Lanche">The Atomic Burger</label><br>
 
@@ -161,7 +173,7 @@
                     <input type="number" id="Quantidade" name="Quantidade">
 
                     <input type="submit" class="centralizado" name="adicionar" value="Adicionar"><br><br>
-                    
+
                 </div>
 
                 <div class="box">
@@ -177,16 +189,17 @@
 
                     <input type="submit" class="centralizado" name="adicionar" value="Adicionar"><br><br>
 
-                   
+
                 </div>
                 <div class="box2">
                     <input type="submit" class="centralizado2" name="adicionar" value="Adicionar"><br><br>
-                   
+
                     <input type="submit" class="centralizado2" name="pedido" value="Realizar Pedido">
-                </div> 
-                
+                </div>
+
             </div>
         </form>
-</header>
+    </header>
 </body>
+
 </html>
